b527978896e6866b61cd70c3cafed449ccf9034d
root <guibayer2@gmail.com>
2020-04-16 13:31:49 -0300

    Corrigindo problema nas inferiores

--- Trapezoidal.java
+++ Trapezoidal.java
@@ -21,55 +21,91 @@ public class Trapezoidal {
 				this.b.getX() + ", " +
 				this.c.getX() + ", " + 
 				this.d.getX() + ") e uma constante c = " + this.constanteC);
-		System.out.println("A função trapezoidal inferior terá pontos: (" + 
-				this.a.getInf() + ", " +
-				this.b.getInf() + ", " +
-				this.c.getInf() + ", " + 
-				this.d.getInf() + ") " );
 		System.out.println("A função trapezoidal superior terá pontos: (" + 
 				this.a.getSup() + ", " +
 				this.b.getSup() + ", " +
 				this.c.getSup() + ", " + 
 				this.d.getSup() + ") " );
+		System.out.println("A função trapezoidal inferior terá pontos: (" + 
+				this.a.getInf() + ", " +
+				this.b.getInf() + ", " +
+				this.c.getInf() + ", " + 
+				this.d.getInf() + ") " );
 	}
 	
 	public void calculaSuperiores() {
-		this.a.setSup(avaliaPontoDaRetaSuperior(this.a.getX(), true));
-		this.b.setSup(avaliaPontoDaRetaSuperior(this.b.getX(), true));
-		this.c.setSup(avaliaPontoDaRetaSuperior(this.c.getX(), false));
-		this.d.setSup(avaliaPontoDaRetaSuperior(this.d.getX(), false));
+		this.a.setSup(this.a.getX() - this.constanteC/2);
+		this.b.setSup(this.b.getX() - this.constanteC/2);
+		this.c.setSup(this.c.getX() + this.constanteC/2);
+		this.d.setSup(this.d.getX() + this.constanteC/2);
 	} 
 	
 	public void calculaInferiores() {
-		this.a.setInf(avaliaPontoDaRetaInferior(this.a.getX(), true));
+		this.a.setInf(this.a.getX() + this.constanteC/2);
 		this.b.setInf(avaliaPontoDaRetaInferior(this.b.getX(), true));
 		this.c.setInf(avaliaPontoDaRetaInferior(this.c.getX(), false));
-		this.d.setInf(avaliaPontoDaRetaInferior(this.d.getX(), false));
+		this.d.setInf(this.d.getX() - this.constanteC/2);
 	} 
-	
-	public float avaliaPontoDaRetaSuperior(float x, boolean primeiraReta) {
-		// Equação superior:
-		// sup_u(x) = min[u(x) + c/2, 1.0]
-		float valorDeRetorno;
-		if(primeiraReta) {
-			valorDeRetorno = x - minF((avaliaPontoDaReta(x) + constanteC), (float) 1.0);
-		}else {
-			valorDeRetorno = x + minF((avaliaPontoDaReta(x) + constanteC), (float) 1.0);
-		}
-		
-		
-		return valorDeRetorno;
-	}
-	
+
 	public float avaliaPontoDaRetaInferior(float x, boolean primeiraReta) {
 		// Equação inferior:
 		// inf_u(x) = min[max[u(x) - c/2, 0], 1.0 - c]
 		float valorDeRetorno = 0;
 		if(primeiraReta) {
-			valorDeRetorno = x + minF(maxF((avaliaPontoDaReta(x) - constanteC/2), 0), ((float) 1.0 - constanteC));
+			//valorDeRetorno = x + minF(maxF((avaliaPontoDaReta(x) - constanteC/2), 0), ((float) 1.0 - constanteC));
+			//valorDeRetorno = x + constanteC/2;
+			valorDeRetorno = this.achaPontoXInferiorDaEq1();
 		}else {
-			valorDeRetorno = x - minF(maxF((avaliaPontoDaReta(x) - constanteC/2), 0), ((float) 1.0 - constanteC));
+			//valorDeRetorno = x - minF(maxF((avaliaPontoDaReta(x) - constanteC/2), 0), ((float) 1.0 - constanteC));
+			//valorDeRetorno = x - constanteC/2;
+			valorDeRetorno = this.achaPontoXInferiorDaEq2();
 		}
+		
+		return valorDeRetorno;
+	}
+	
+	//Para descobrir o ponto X baseado no Y;
+	
+	// x*a2 + a1*b2 + b1*y - y*a1 - a2*b1 - b2*x = 0
+	// x*a2 - b2*x = -a1*b2 - b1*y + y*a1 + a2*b1
+	// (a2 - b2) * x = (-b1 + a1) * y + (-a1*b2 + a2*b1)
+	// x = (-b1 + a1) * y + (-a1*b2 + a2*b1) / (a2 - b2)
+	
+	public float achaPontoXInferiorDaEq1() {
+		float valorDeRetorno = 0; // X = 0
+		float y = 1 - this.constanteC/2; // valor de Y(Complemento da constanteC)
+		float ValorDivisorReta1 = 0, valorFixoEqDaReta1 = 0, yEqDaReta1 = 0;
+		
+		
+		// x = (-b1 + a1) * y + (-a1*b2 + a2*b1) / (a2 - b2)
+		yEqDaReta1 = (a.getX() - b.getX()) * y; // (a1 - b1) * y	
+		valorFixoEqDaReta1 = -a.getX()*b.getY() + a.getY()*b.getX(); //(-a1*b2 + a2*b1)
+		ValorDivisorReta1 = a.getY() - b.getY(); // (a2 - b2)
+		
+		
+		valorDeRetorno = yEqDaReta1 + valorFixoEqDaReta1 / ValorDivisorReta1;
+		
+		//valorDeRetorno = valorDeRetorno - this.constanteC/2;
+		
+		return valorDeRetorno;
+	}
+	
+	public float achaPontoXInferiorDaEq2() {
+		float valorDeRetorno = 0; // X = 0
+		float y = 1 - this.constanteC/2; // valor de Y(Complemento da constanteC)
+		float ValorDivisorReta2 = 0, valorFixoEqDaReta2 = 0, yEqDaReta2 = 0;
+		
+		
+		// x = (a1 - b1) * y + (-a1*b2 + a2*b1) / (a2 - b2)
+		yEqDaReta2 = (c.getX() - d.getX()) * y; // (b1 - a1) * y
+		valorFixoEqDaReta2 = -c.getX()*d.getY() + c.getY()*d.getX();//(-a1*b2 + a2*b1)
+		ValorDivisorReta2 = c.getY() - d.getY(); // (a2 - b2)
+		
+		
+		valorDeRetorno = yEqDaReta2 + valorFixoEqDaReta2 / ValorDivisorReta2;
+		
+		//valorDeRetorno = valorDeRetorno + this.constanteC/2;
+		
 		
 		return valorDeRetorno;
 	}
@@ -88,7 +124,7 @@ public class Trapezoidal {
 		xEqDaReta2 = d.getY() - c.getY();
 		valorFixoEqDaReta2 = c.getX()*d.getY() + c.getY()*d.getX();
 		
-		if (x >= a.getX() && x <= b.getX()){
+		/*if (x >= a.getX() && x <= b.getX()){
 			valorDeRetorno = (xEqDaReta1 * x + valorFixoEqDaReta1) / yEqDaReta1;
 		}else if (x >= c.getX() && x <= d.getX()){
 			valorDeRetorno = (xEqDaReta2 * x + valorFixoEqDaReta2) / yEqDaReta2;
@@ -96,7 +132,7 @@ public class Trapezoidal {
 			valorDeRetorno = 1;
 		}else {
 			valorDeRetorno = 0;
-		}
+		}*/
 		
 		//System.out.println("Para o valor de x: " + x + " o y correspondente será: " + valorDeRetorno);
 		
--- Type1ToType2.java
+++ Type1ToType2.java
@@ -5,15 +5,29 @@ public class Type1ToType2 {
 	// b1 b2
 	// x  y
 	
+	// Para descobrir o ponto Y:
 	// x*a2 + a1*b2 + b1*y - y*a1 - a2*b1 - b2*x = 0
 	// b1*y - y*a1 = -x*a2 - a1*b2 + a2*b1 + b2*x
 	// (b1 - a1) * y = (b2 - a2) * x + (-a1*b2 + a2*b1)
 	// y = ((b2 - a2) * x + (a1*b2 + a2*b1)) / (b1 - a1)
 	
+	//Para descobrir o ponto X baseado no Y;
+	
+	// x*a2 + a1*b2 + b1*y - y*a1 - a2*b1 - b2*x = 0
+	// x*a2 - b2*x = -a1*b2 - b1*y + y*a1 + a2*b1
+	// (a2 - b2) * x = (-b1 + a1) * y + (-a1*b2 + a2*b1)
+	// x = (-b1 + a1) * y + (-a1*b2 + a2*b1) / (a2 - b2)
+	
+	
 	
 	public static void main(String args[]) {
-		Trapezoidal lowROA = new Trapezoidal(0, 0, 2, 4, (float) 0.2);
-		lowROA.printValoresResultantes();
+		float constanteC = (float) 3;
+		Trapezoidal highROA = new Trapezoidal((float) 0, (float) 0, (float) 2, (float) 4, constanteC);
+		Trapezoidal mediumROA = new Trapezoidal((float) 2, (float) 4, (float) 4, (float) 6, constanteC);
+		Trapezoidal lowROA = new Trapezoidal((float) 4, (float) 6, (float) 10, (float) 10, constanteC);
+		highROA.printValoresResultantes();
+		//lowROA.printValoresResultantes();
+		
 		//System.out.println("Com a entrada 8.45 o valor de y é: " + lowPromotion.avaliaPontoDaReta((float) 8.45));
 		//System.out.println("Superior com entrada 8.45 é: " + lowPromotion.avaliaPontoDaRetaSuperior((float) 8.45));
 		//System.out.println("Inferior com entrada 8.45 é: " + lowPromotion.avaliaPontoDaRetaInferior((float) 8.45));
